19b9d724a7859701b3f4425126a16384262581c8
MERON G. TEKIE <ramenimo@MERONs-MacBook-Pro.local>
2022-05-12 14:23:25 -0700

    Separate businness Logics in different files

--- ToDoList/Controllers/HomeController.cs
+++ ToDoList/Controllers/HomeController.cs
@@ -7,22 +7,9 @@ namespace ToDoList.Controllers
   public class HomeController : Controller
   {
     [HttpGet("/")]
-    public ActionResult Index()
-    {
-    List<Item> allItems =Item.GetAll();
-    return View(allItems);
-    }
-    [HttpGet("/items/new")]
     public ActionResult CreateForm()
-    {
-      return View();
-    }
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
-    {
-      Item myItem = new Item(description);
-      // return View("Index",myItem);
-      return RedirectToAction("Index");
+    {   
+    return View();
     }
   }
 }
